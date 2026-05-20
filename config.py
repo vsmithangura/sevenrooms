de86d7b0a11f078b7b95698a90d984d71b4cb4c3
vsmithangura <vincesmithangura.social@gmail.com>
2026-05-20 00:08:54 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,41 @@
+TELEGRAM_CHAT_ID = "8802571752"  # Your Telegram chat id. Example: -192713119
+BOT_SECRET = "bot8227040691:AAHoYB2avFEUtU32kBLQhj5nFGTsLb2XoP4"  # Your bot secret. Example: bot18123912371:AG0FJAP7lXjWgvASDAjE48qP324JJSKO0EkASD
+ENABLE_EMAIL = False
+EMAIL_USERNAME = ""  # Your email username. Example: test@example.com
+EMAIL_PASSWORD = ""  # Your email password. Example: password
+EMAIL_SMTP_SERVER = "smtp.gmail.com"  # Your email smtp server. Example: smtp.gmail.com
+EMAIL_SMTP_PORT = 587  # Your email smtp port. Example: 587
+EMAIL_TO = ""  # Email address to notify. Example: email@example.com
+NUM_PEOPLE = 2  # Number of people you want the booking for. Example: 2
+MAIN_TIME = "19:00"  # Main time to look for. Example: 19:15 (24hr format)
+TIMES_NEEDED = [
+    "17:00:00",
+    "17:15:00",
+    "17:30:00",
+    "18:00:00",
+    "18:15:00",
+    "18:30:00",
+    "18:45:00",
+    "19:00:00",
+    "19:15:00",
+    "19:30:00",
+    "19:45:00",
+    "20:00:00",
+    "20:15:00",
+    "20:30:00",
+    "20:45:00",
+    "21:00:00",
+    "21:15:00",
+    "21:30:00"
+]  # Alternative time slots you want look for. Example: ['19:00:00', '19:30:00', '19:45:00'] (24hr format)
+DATES_NEEDED = [
+    "2026-05-21",
+    "2026-05-22",
+    "2026-05-23",
+    "2026-05-24",
+    "2026-05-25",
+    "2026-05-26",
+    "2026-05-28"
+]  # Dates you want to look for. Example: ['2021-10-01', '2021-10-02'] (YYYY-MM-DD format)
+VENUE = "oyatte"  # Venue name/ Restaurant ID (FOLLOW how to get restuarant ID from readme for steps). Example: sticksnsushicoventgarden
+RETRY_AFTER = 120  # seconds to wait before checking again
